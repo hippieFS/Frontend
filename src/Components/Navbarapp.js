1716e4163b62ded46f9eef511fe8c7d2c4f2e420
prnvsan <pranavhnairxd@gmail.com>
2022-07-17 00:28:20 +0530

    added cards

--- src/Components/Navbarapp.js
+++ src/Components/Navbarapp.js
@@ -3,9 +3,29 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Card from './Card.js';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import food from "../assets/food.jpg";
+
+import "./Navbarapp.css";
 
 function Navbarapp() {
+  const recipeAuthor = "Varoon";
+  const recipeItem = {
+    title: "Takobell",
+    date: "8 July, 2022",
+    image: food,
+    description:
+      "Me like tacos.Tacos good. They are great. They are nice. Eat tacos. ",
+  };
+  
+  const like= 193;
+  const isLiked = true;
+
   return (
+   <div>
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
@@ -25,6 +45,32 @@ function Navbarapp() {
           </Nav>
         </Container>
       </Navbar>
+     <Container>
+      <Row className="rowone">
+        <Col md={{ span: 7, offset: 4 }}>
+      <Card author={recipeAuthor}
+          title={recipeItem.title}
+          date={recipeItem.date}
+          description={recipeItem.description}
+          liked={isLiked}
+          likeCount={like}/>
+          </Col>
+          </Row>
+
+          <Row className="rowtwo">
+        <Col md={{ span: 7, offset: 4 }}>
+      <Card author={recipeAuthor}
+          title={recipeItem.title}
+          date={recipeItem.date}
+          description={recipeItem.description}
+          liked={isLiked}
+          likeCount={like}/>
+          </Col>
+          </Row>
+
+      </Container>
+      </div>
+      
   );
 }
 
